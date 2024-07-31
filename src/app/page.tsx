1be083ec23b301ae102a48431fbc5cc189858e13
hrg <hamidrezaghaseme@gmail.com>
2024-07-31 16:44:46 +0330

    init app

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,39 +1,52 @@
-'use client';
+"use client";
 
-import { Section, Cell, Image, List } from '@telegram-apps/telegram-ui';
+import {
+  Section,
+  Cell,
+  Image,
+  List,
+  Placeholder,
+} from "@telegram-apps/telegram-ui";
 
-import { Link } from '@/components/Link/Link';
+import { Link } from "@/components/Link/Link";
 
-import tonSvg from './_assets/ton.svg';
+import dragon from "./_assets/dragon.jpg";
 
 export default function Home() {
   return (
     <List>
-      <Section
-        header='Features'
-        footer='You can use these pages to learn more about features, provided by Telegram Mini Apps and other useful projects'
-      >
-        <Link href='/ton-connect'>
-          <Cell
-            before={<Image src={tonSvg.src} style={{ backgroundColor: '#007AFF' }}/>}
-            subtitle='Connect your TON wallet'
-          >
+      <Section header="Dragon" footer="This is a test application.">
+        <Placeholder header="Dragon App" description="The dragon enters">
+          <img
+            alt="Telegram sticker"
+            src={dragon.src}
+            style={{ display: "block", width: "100%", height: "144px" }}
+          />
+        </Placeholder>
+        <Link href="/ton-connect">
+          <Cell before={<div />} subtitle="Connect your TON wallet">
             TON Connect
           </Cell>
         </Link>
       </Section>
       <Section
-        header='Application Launch Data'
-        footer='These pages help developer to learn more about current launch information'
+        header="Application Launch Data"
+        footer="These pages help developer to learn more about current launch information"
       >
-        <Link href='/init-data'>
-          <Cell subtitle='User data, chat information, technical data'>Init Data</Cell>
+        <Link href="/init-data">
+          <Cell subtitle="User data, chat information, technical data">
+            Init Data
+          </Cell>
         </Link>
-        <Link href='/launch-params'>
-          <Cell subtitle='Platform identifier, Mini Apps version, etc.'>Launch Parameters</Cell>
+        <Link href="/launch-params">
+          <Cell subtitle="Platform identifier, Mini Apps version, etc.">
+            Launch Parameters
+          </Cell>
         </Link>
-        <Link href='/theme-params'>
-          <Cell subtitle='Telegram application palette information'>Theme Parameters</Cell>
+        <Link href="/theme-params">
+          <Cell subtitle="Telegram application palette information">
+            Theme Parameters
+          </Cell>
         </Link>
       </Section>
     </List>
